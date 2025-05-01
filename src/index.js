a37bc1cf7cf4d613e61d6e7c682e68a16f325d16
DustUnderBag <usamary55@gmail.com>
2025-05-01 15:38:40 -0400

    Complete function fetching data from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import "./reset.css";
 
 console.log("Script entry point working");
+
+/*
+    Needed data:
+    Location
+    Weather condition at the moment
+    Temperature
+    Min & max temperature
+    Feels like temperature
+    Description of the day
+    
+    Hourly forecast of today
+    - List hours from NOW to the next 24 hours e.g. Now(1am) to next 1am
+    - weather and temperature of that hour.
+    - get icon
+    
+    Daily Forecast
+    - On the top, display description of the next few days.
+    - List the following 9 days.(from today to 9 days later).
+    - weather condition
+    - min & max temperature
+    
+*/
+
+async function fetchData(location) {
+  const key = "8UT7GXF3M33L57GR7VV2CNKCP";
+  const contentType = "json";
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${key}&contentType=${contentType}`;
+
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  const json = await response.json();
+  console.log(json);
+}
+
+fetchData("Tokyo");
