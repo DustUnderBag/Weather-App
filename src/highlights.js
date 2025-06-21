4a266a5064a7fe72941d7cfa6aaf2987ff27edcf
DustUnderBag <usamary55@gmail.com>
2025-06-21 17:56:38 -0400

    Complete styles of uvindex pie chart display
    Implemented an animated pie chart visualizing UV
    index.
    
    The chart ranges from 0 to 10, with numberings on
    the side. The yellow bar rotates in proportion to
    the UV index.

--- src/highlights.js
+++ src/highlights.js
@@ -1,21 +1,22 @@
 import { weatherData } from "./fetcher";
 
+/* 
+  UV Index Rating
+  0-2 = low, 
+  3-5 = Moderate, 
+  6-7 = high, 
+  8-10 = Very high, 
+  11 = Extreme
+*/
 export function displayHighlights() {
   const data = weatherData.currentConditions;
 
-  /*
-    const uv = data.uvindex;
-    const windSpeed = data.windspeed;
-    const windDirection = data.winddir;
-    const sunrise = data.sunrise;
-    const sunset = data.sunset;
-    const humidity = data.humidity;
-    const visibility = data.visibility;
-    const airQuality = data.aqius; //US rating;
-    */
+  const uv = document.querySelector("#uv-chart");
+  uv.setAttribute("data-content", data.uvindex);
 
-  const uv = document.querySelector("#uv > span");
-  uv.textContent = data.uvindex;
+  const uvBar = document.querySelector("#uv-chart #uv-bar");
+  const rotateDeg = getUvBarRotateDeg(data.uvindex);
+  uvBar.style.transform = `rotate(${rotateDeg})`;
 
   const windSpeed = document.querySelector("#wind-speed");
   windSpeed.textContent = data.windspeed + "km/h";
@@ -37,4 +38,10 @@ export function displayHighlights() {
 
   const airQuality = document.querySelector("span#air-quality");
   airQuality.textContent = data.aqius;
+}
+
+function getUvBarRotateDeg(uvIndex) {
+  if (uvIndex > 10) uvIndex = 10;
+  const deg = (uvIndex / 10) * 180;
+  return deg + "deg";
 }
